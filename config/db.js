1bb0c587f9f6f9c457e273131d45b957eb1ae574
CiriloJr6 <cirilojunior2000@gmail.com>
2026-02-06 17:30:19 +0200

    Ajustando conexão para variável de ambiente

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-    module.exports = {mongoURI: 'mongodb+srv://cirilojunior_db_user:KFOUAeaJSwwNU2AR@blogapp.d0lncmn.mongodb.net/?appName=blogapp'}
+    module.exports = {mongoURI: 'mongodb+srv://cirilojunior_db_user:cj12345678@blogapp.d0lncmn.mongodb.net/?appName=blogapp'}
 }else{
     module.exports = {mongoURI: 'mongodb://localhost/blogapp'};
 }
