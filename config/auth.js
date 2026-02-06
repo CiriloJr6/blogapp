5e3a8a1cb4d9ebca0cec9d58f77d30c64b5fc49b
CiriloJr6 <cirilojunior2000@gmail.com>
2026-02-06 15:55:28 +0200

    initial commit

---
+++ config/auth.js
@@ -0,0 +1,43 @@
+const localStrategy = require('passport-local').Strategy;
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+//Model de usuário
+require('../models/Usuario');
+const Usuario = mongoose.model('usuario');
+
+module.exports = function (passport) {
+
+    passport.use(new localStrategy({ usernameField: 'email', passwordField: 'senha' }, (email, senha, done) => {
+
+        Usuario.findOne({ email: email }).then((usuario) => {
+            if (!usuario) {
+                return done(null, false, { message: "Essa conta não existe" });
+            }
+
+            bcrypt.compare(senha, usuario.senha, (err, isMatch) => {
+
+                if (isMatch) {
+                    return done(null, usuario);
+                } else {
+                    return done(null, false, { message: "Senha incorreta" });
+                }
+            })
+        })
+    }))
+
+    passport.serializeUser((usuario, done) => {
+        done(null, usuario.id);
+    })
+
+    passport.deserializeUser((id, done) => {
+    Usuario.findById(id)
+        .then((user) => {
+            done(null, user);
+        })
+        .catch((err) => {
+            done(err, null);
+        });
+});
+
+}
